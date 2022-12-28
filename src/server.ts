652b3133ba0030e3740448ec6faaf206577bf33a
rafael.lisboa-at-428417797992 <rafael.lisboa@trigg.com.br>
2022-12-28 12:39:47 -0300

    feat: adding the app to docker-compose

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,20 @@ import { errorMiddleware } from './middlewares/error';
 appDataSource.initialize()
   .then(() => {
     const app = express();
+    
     app.use(express.json());
     app.use(cors());
     app.use(router);
     app.use(errorMiddleware)
+    
     const server = app.listen(process.env.APP_PORT, () => {
       console.log('Server runnnig');
     })
+    
     process.on('SIGTERM', shutDown);
     process.on('SIGINT', shutDown);
+
+
     function shutDown() {
       console.log('Received kill signal, shutting down gracefully');
       server.close(() => {
